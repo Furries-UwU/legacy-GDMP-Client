d1a3a8613204043293e41681d73f566319a615f9
xhayper <hayper1919@gmail.com>
2022-05-09 13:42:48 +0700

    feat: Stuff

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,28 +1,40 @@
 #include "dllmain.hpp"
 
-BOOL WINAPI DllMain(
-    HINSTANCE hinstDLL,
-    DWORD fdwReason,
-    LPVOID lpReserved) {
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    if (fdwReason != DLL_PROCESS_ATTACH) return FALSE;
 
-    switch (fdwReason)
+#ifdef SHOW_DEBUG_CONSOLE
+    AllocConsole();
+    SetConsoleTitle(LPCSTR("Geometry Dash"));
+
+    // Redirect cout, cin and cerr
+    FILE* dummy;
+    freopen_s(&dummy, "CONOUT$", "w", stdout);
+    freopen_s(&dummy, "CONOUT$", "w", stderr);
+    freopen_s(&dummy, "CONIN$", "r", stdin);
+#endif
+
+    if (enet_initialize() != 0)
     {
-    case DLL_PROCESS_ATTACH:
-        // Initialize once for each new process.
-        // Return FALSE to fail DLL load.
-        break;
-
-    case DLL_THREAD_ATTACH:
-        // Do thread-specific initialization.
-        break;
-
-    case DLL_THREAD_DETACH:
-        // Do thread-specific cleanup.
-        break;
-
-    case DLL_PROCESS_DETACH:
-        // Perform any necessary cleanup.
-        break;
+        fmt::print("An error occurred while initializing ENet.");
+        return FALSE;
     }
-    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+    atexit(enet_deinitialize);
+
+    ENetHost* client;
+    client = enet_host_create(NULL,
+        1,
+        1,
+        0,
+        0);
+
+    if (client == NULL)
+    {
+        fmt::print("An error occurred while trying to create an ENet client host.");
+        exit(EXIT_FAILURE);
+    }
+
+    enet_host_destroy(client);
+
+    return TRUE;
 }
--- src/dllmain.hpp
+++ src/dllmain.hpp
@@ -11,4 +11,3 @@
 
 #include <cocos2d.h>
 using namespace cocos2d;
-
