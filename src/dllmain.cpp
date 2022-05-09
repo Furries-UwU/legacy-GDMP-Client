365b8a7eae53362d77dd41cfa2134988729c9654
xhayper <hayper1919@gmail.com>
2022-05-09 12:31:23 +0700

    feat: Fix CMake again, removed megahack, used minhook

---
+++ src/dllmain.cpp
@@ -0,0 +1,30 @@
+#include <enet/enet.h>
+#include <fmt/core.h>
+#include <gd.h>
+
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,
+    DWORD fdwReason,
+    LPVOID lpReserved) {
+
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        // Initialize once for each new process.
+        // Return FALSE to fail DLL load.
+        break;
+
+    case DLL_THREAD_ATTACH:
+        // Do thread-specific initialization.
+        break;
+
+    case DLL_THREAD_DETACH:
+        // Do thread-specific cleanup.
+        break;
+
+    case DLL_PROCESS_DETACH:
+        // Perform any necessary cleanup.
+        break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
