3fd46769187e522c559dec161bc0185fe22e7a48
xhayper <hayper1919@gmail.com>
2022-05-16 22:31:30 +0700

    feat: Help

--- src/main.cpp
+++ src/main.cpp
@@ -117,9 +117,9 @@ GEODE_API bool GEODE_CALL geode_load(Mod *mod) {
     atexit(enet_deinitialize);
 
     Global *global = Global::get();
-    global->host = enet_host_create(nullptr, 1, 2, 0, 0);
+    global->host = enet_host_create(nullptr, 1, 1, 0, 0);
 
-    connect("192.168.1.33", 23973);
+    connect("127.0.0.1", 23973);
 
     std::thread eventThread(&pollEvent);
     eventThread.detach();
